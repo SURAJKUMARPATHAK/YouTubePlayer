8767fcf8bd581bf23717b39b3388d82489a328f4
SURAJKUMARPATHAK <30904558+SURAJKUMARPATHAK@users.noreply.github.com>
2018-08-21 17:23:38 +0530

    Initial commit

---
+++ app/src/main/java/com/my/youtubeplayer/Config.java
@@ -0,0 +1,12 @@
+package com.my.youtubeplayer;
+
+
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyB_ODgfTPKW4_h6mUQRkQg2qaG8UiB6-0E";
+
+}
